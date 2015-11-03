a608f5cd9b906939b8bc778b35ef98b4c776ec50
Matt Weeks <m_weeks@hotmail.co.uk>
2015-11-03 11:48:52 +0000

    Update index text.

--- index.php
+++ index.php
@@ -18,13 +18,13 @@
                 <div class="col-md-12 content">
                     <h2>Hello!</h2>
                     <p>
-                    	I'm Matt Weeks, a student at the University of Kent Canterbury currently working for Cisco in California.<br><br>
+                    	I'm Matt Weeks, a postgraduate student studying for an MSc in “Advanced Computer Science” at the University of Kent, Canterbury.<br><br>
 
                     	I'm a programmer/web developer with experience programming in multiple languages including HTML, JavaScript, Python, Java and more, as well as using all the major operating systems.<br><br>
 
-                    	At Cisco, I'm currently working in the OpenStack team improving network visibility to improve the efficiency of network operations.<br><br>
+                    	At Cisco, I worked in the OpenStack team creating tools to increase network visibility to improve the efficiency of network operations.<br><br>
 
-                    	Outside of work my main hobbies are playing/listening to music, tennis, skiing, mountain biking and photography (you can take a look at some of my photos on the Photography tab.).<br><br>
+                    	Outside of work my main hobbies are playing/listening to music, skiing, mountain biking and photography. You can take a look at some of my photos on the Photography tab.<br><br>
 
                     	For details of the courses I've been taking at University please check out my Linkedin <a href="http://www.linkedin.com/in/mweeks1" target="_blank">profile</a>.
                     </p>
